8cbdb58010fac61e94935d0e5e1ac9cf8c426c1c
sara farahani <99091575+mrssarafarahani@users.noreply.github.com>
2022-04-16 00:45:38 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["iranyekan"],
+            }
+        },
+    },
+    plugins: [],
+}
